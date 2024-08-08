8e9324f37cb8187bb9a42a78d597a8527dee263e
Jesusrac31 <jesusraceros@gmail.com>
2024-08-08 19:03:27 +0200

    8-8-2024-19:03

---
+++ Cuadernillo de la fase local de la UCM 2020/H. Choques en el carril bici.cpp
@@ -0,0 +1,218 @@
+//Librerías incluidas en #include<bitstdc++.h>
+#include<algorithm>
+#include<array>
+#include<atomic>
+#include<bitset>
+#include<ccomplex>
+#include<cerrno>
+#include<cfenv>
+#include<cfloat>
+#include<chrono>
+#include<cinttypes>
+#include<ciso646>
+#include<climits>
+#include<clocale>
+#include<cmath>
+#include<complex>
+#include<condition_variable>
+#include<csetjmp>
+#include<csignal>
+#include<cstdalign>
+#include<cstdarg>
+#include<cstdbool>
+#include<cstddef>
+#include<cstdint>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<ctgmath>
+#include<ctime>
+#include<cwchar>
+#include<cwctype>
+#include<deque>
+#include<exception>
+#include<forward_list>
+#include<fstream>
+#include<functional>
+#include<future>
+#include<initializer_list>
+#include<iomanip>
+#include<ios>
+#include<iosfwd>
+#include<iostream>
+#include<istream>
+#include<iterator>
+#include<limits>
+#include<list>
+#include<locale>
+#include<map>
+#include<memory>
+#include<mutex>
+#include<new>
+#include<numeric>
+#include<ostream>
+#include<queue>
+#include<random>
+#include<ratio>
+#include<regex>
+#include<scoped_allocator>
+#include<set>
+#include<sstream>
+#include<stack>
+#include<stdexcept>
+#include<streambuf>
+#include<string>
+#include<system_error>
+#include<thread>
+#include<tuple>
+#include<type_traits>
+#include<typeindex>
+#include<typeinfo>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<valarray>
+#include<vector>
+#include <stdlib.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<long long int> vll;
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef map<string, int> msi;
+typedef map<int, vector<int>> miv;
+
+// Funciones vector
+#define PB(a) push_back(a);
+
+bool sort_func(pii a, pii b) {
+  if (a.first < b.first) {
+    return true;
+  } else {
+    return false;
+  }
+}
+#define ord(vect) sort(vect.begin(), vect.end(), sort_func);
+
+#define borra_el(vect, el) vect.erase(vect.find(el));
+#define borra_range(vect, a, b) vect.erase(a, b);
+#define borra(vect, n) vect.erase(vect.begin() + n);
+#define B begin();
+#define E end();
+#define copia(v1, v2)                                                          \
+  ;                                                                            \
+  copy(v1.begin(), v1.end(), back_inserter(v2));
+
+// Funciones map
+#define F first;
+#define S second;
+
+// Logaritmo de 2
+double log_2 = log(2);
+double log2(int a) { return (log(a) / log_2); }
+
+void Imprime(vi vect) {
+  for (int i = 0; i < vect.size(); i++) {
+    cout << vect[i] << " ";
+  }
+  cout << "\n";
+}
+
+void Imprime2d(vector<vi> vect) {
+  for (int j = 0; j<vect.size(); j++){
+    for (int i = 0; i < vect[j].size(); i++) {
+        cout << vect[j][i] << " ";
+    }
+    cout << "\n";
+  }
+}
+
+void Imprime_set(set<int> s) {
+  copy(s.begin(), s.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;
+}
+
+int maximo_comun_divisor(int a, int b) {
+  int temporal; // Para no perder b
+  while (b != 0) {
+    temporal = b;
+    b = a % b;
+    a = temporal;
+  }
+  return a;
+}
+
+int minimo_comun_multiplo(int a, int b) {
+  return (a * b) / maximo_comun_divisor(a, b);
+}
+
+bool isNumeric(string const &str) {
+  auto it = str.begin();
+  while (it != str.end() && isdigit(*it)) {
+    it++;
+  }
+  return !str.empty() && it == str.end();
+}
+
+vi lee(int n) {
+  int el;
+  vi vect;
+  for (int i = 0; i < n; i++) {
+    cin >> el;
+    vect.PB(el);
+  }
+  return (vect);
+}
+
+bool solve() {
+  // Code aquí
+  int n;
+  cin >> n;
+  if (n == 0){
+    return false;
+  }
+  vector<pii> v;
+  pii par;
+  int el;
+  for (int i = 0; i<n; i++){
+    cin >> el;
+    par.first = el;
+    cin >> el;
+    par.second = el;
+    v.PB(par);
+  }
+  sort(v.begin(), v.end());
+  int sol = INT_MAX;
+  int caso;
+  for (int i = 1; i<n; i++){
+    if (v[i].first-v[i-1].first == 0){
+        sol = 0;
+        break;
+    }
+    if (v[i].second-v[i-1].second != 0){
+        caso = (v[i-1].first-v[i].first)/(v[i].second-v[i-1].second);
+        if (((v[i-1].first-v[i].first) > 0 && (v[i].second-v[i-1].second) > 0) || ((v[i-1].first-v[i].first) < 0 && (v[i].second-v[i-1].second) < 0)){
+            sol = min(sol, caso);
+        }
+    }
+  }
+  if (sol == INT_MAX){
+    cout << "SIN COLISION" << endl;
+  } else {
+    cout << sol << endl;
+  }
+  
+  return true;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr); 
+  bool T = true;
+  while (T) {
+    T = solve();
+  }
+  return 0;
+}
---
+++ Cuadernillo de la fase local de la UCM 2020/I. Megaautopista de Oriente.cpp
@@ -0,0 +1,213 @@
+//Librerías incluidas en #include<bitstdc++.h>
+#include<algorithm>
+#include<array>
+#include<atomic>
+#include<bitset>
+#include<ccomplex>
+#include<cerrno>
+#include<cfenv>
+#include<cfloat>
+#include<chrono>
+#include<cinttypes>
+#include<ciso646>
+#include<climits>
+#include<clocale>
+#include<cmath>
+#include<complex>
+#include<condition_variable>
+#include<csetjmp>
+#include<csignal>
+#include<cstdalign>
+#include<cstdarg>
+#include<cstdbool>
+#include<cstddef>
+#include<cstdint>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<ctgmath>
+#include<ctime>
+#include<cwchar>
+#include<cwctype>
+#include<deque>
+#include<exception>
+#include<forward_list>
+#include<fstream>
+#include<functional>
+#include<future>
+#include<initializer_list>
+#include<iomanip>
+#include<ios>
+#include<iosfwd>
+#include<iostream>
+#include<istream>
+#include<iterator>
+#include<limits>
+#include<list>
+#include<locale>
+#include<map>
+#include<memory>
+#include<mutex>
+#include<new>
+#include<numeric>
+#include<ostream>
+#include<queue>
+#include<random>
+#include<ratio>
+#include<regex>
+#include<scoped_allocator>
+#include<set>
+#include<sstream>
+#include<stack>
+#include<stdexcept>
+#include<streambuf>
+#include<string>
+#include<system_error>
+#include<thread>
+#include<tuple>
+#include<type_traits>
+#include<typeindex>
+#include<typeinfo>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<valarray>
+#include<vector>
+#include <stdlib.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<long long int> vll;
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef map<string, int> msi;
+typedef map<int, vector<int>> miv;
+
+// Funciones vector
+#define PB(a) push_back(a);
+
+bool sort_func(int a, int b) {
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+#define ord(vect) sort(vect.begin(), vect.end(), sort_func);
+
+#define borra_el(vect, el) vect.erase(vect.find(el));
+#define borra_range(vect, a, b) vect.erase(a, b);
+#define borra(vect, n) vect.erase(vect.begin() + n);
+#define B begin();
+#define E end();
+#define copia(v1, v2)                                                          \
+  ;                                                                            \
+  copy(v1.begin(), v1.end(), back_inserter(v2));
+
+// Funciones map
+#define F first;
+#define S second;
+
+// Logaritmo de 2
+double log_2 = log(2);
+double log2(int a) { return (log(a) / log_2); }
+
+void Imprime(vi vect) {
+  for (int i = 0; i < vect.size(); i++) {
+    cout << vect[i] << " ";
+  }
+  cout << "\n";
+}
+
+void Imprime2d(vector<vi> vect) {
+  for (int j = 0; j<vect.size(); j++){
+    for (int i = 0; i < vect[j].size(); i++) {
+        cout << vect[j][i] << " ";
+    }
+    cout << "\n";
+  }
+}
+
+void Imprime_set(set<int> s) {
+  copy(s.begin(), s.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;
+}
+
+int maximo_comun_divisor(int a, int b) {
+  int temporal; // Para no perder b
+  while (b != 0) {
+    temporal = b;
+    b = a % b;
+    a = temporal;
+  }
+  return a;
+}
+
+int minimo_comun_multiplo(int a, int b) {
+  return (a * b) / maximo_comun_divisor(a, b);
+}
+
+bool isNumeric(string const &str) {
+  auto it = str.begin();
+  while (it != str.end() && isdigit(*it)) {
+    it++;
+  }
+  return !str.empty() && it == str.end();
+}
+
+vi lee(int n) {
+  int el;
+  vi vect;
+  for (int i = 0; i < n; i++) {
+    cin >> el;
+    vect.PB(el);
+  }
+  return (vect);
+}
+
+bool solve(int n, int k) {
+  // Code aquí
+  vi dist, cost;
+  dist = lee(n);
+  cost = lee(n);
+
+  //Cada nodo hará referencia al elemento anterior del cual se podrá acceder
+  //Las posibilidades de acceder desde otro nodo anterior la tendremos en cuenta más tarde
+
+  vi nodo_ant(n,-1); //Vector de n elementos fijados a -1
+  int index_ant = -1;
+  for (int i = 0; i<n; i++){
+    while(dist[i] - dist[index_ant+1] >= k){
+        index_ant++;
+    }
+    nodo_ant[i] = index_ant;
+  }
+
+  //Una vez que todos los nodos tienen su nodo anterior asignado, comprobamos todas las posibilidades
+  //Guardaremos en una lista los maximos empezando desde el nodo 0, luego comprobaremos si es más rentable seleccionar dicho nodo o seguir con el beneficio del nodo anterior
+
+  vi beneficios;
+  beneficios.PB(0);
+  for (int i = 0; i<n; i++){
+    if (nodo_ant[i] == -1){
+        beneficios.PB(max(beneficios[i], cost[i]));
+    } else {
+        beneficios.PB(max(beneficios[i], (cost[i] + beneficios[nodo_ant[i]+1])));
+    }
+  }
+  cout << beneficios[n] << endl;
+  
+  return true;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr); 
+  bool T = true;
+  int n, k;
+  while (cin >> n >> k) {
+    T = solve(n, k);
+  }
+  return 0;
+}
